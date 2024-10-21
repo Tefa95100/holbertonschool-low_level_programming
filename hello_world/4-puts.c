5ea7a20e834a3d4c61fa364d754453b08e89c1c7
Tefa95100 <damien.julien.95100@gmail.com>
2024-10-21 15:29:25 +0200

    Add betty documentation

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - Function main necessary for start the programm
+ * @void: nothing
+ * Return: return 0
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
