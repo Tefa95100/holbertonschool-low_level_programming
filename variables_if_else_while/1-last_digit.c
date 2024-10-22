cb0049a2d29133b67133824a05ca6d1fcb370013
Tefa95100 <damien.julien.95100@gmail.com>
2024-10-22 17:46:48 +0200

    correction bad variable and add new line

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -14,15 +14,15 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	l = n % 10;
-	if (f == 0)
+	if (l == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n);
 	}
-	else if (f > 5)
+	else if (l > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n);
 	}
 	else
-		printf("Last digit of %d is %d and is less than 6 and not 0", n);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n);
 	return (0);
 }
