f994839c13528f7a5e4f64cf789aa4f944aba6c3
Tefa95100 <damien.julien.95100@gmail.com>
2024-10-23 11:58:29 +0200

    Add exercise 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	while (i <= 9)
+	{
+		putchar('0' + i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
