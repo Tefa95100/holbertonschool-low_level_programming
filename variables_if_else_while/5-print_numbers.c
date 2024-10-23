7a862484be9231f84c7ed8166c2cea16ceb028de
Tefa95100 <damien.julien.95100@gmail.com>
2024-10-23 11:21:22 +0200

    Add exercise 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	a = 0;
+	while (a <= 9)
+	{
+		printf("%d", a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
