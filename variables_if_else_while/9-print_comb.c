dc8fabb054bffdebe532e2065e66787a4881c672
Tefa95100 <damien.julien.95100@gmail.com>
2024-10-23 16:41:01 +0200

    Add exercise 9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	while (i <= 9)
+	{
+		putchar('0' + i);
+		putchar(',');
+		putchar(' ');
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
