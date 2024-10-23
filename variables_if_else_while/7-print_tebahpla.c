e59f7de4f8c78b0f5c828b9365d7ed77353b5e19
Tefa95100 <damien.julien.95100@gmail.com>
2024-10-23 14:48:30 +0200

    Add exercise 7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: return 0
+ */
+int main(void)
+{
+	int a;
+
+	a = 122;
+	while(a >= 97)
+	{
+		putchar(a);
+		a--;
+	}
+	putchar('\n');
+	return (0);
+}
