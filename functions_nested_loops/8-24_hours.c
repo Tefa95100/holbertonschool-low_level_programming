61bffda641464870f8f6cd2a3a28dfc1a66f0030
Tefa95100 <damien.julien.95100@gmail.com>
2024-10-25 20:22:26 +0200

    Add a +0

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -20,11 +20,11 @@ void jack_bauer(void)
 				{
 					if (a != 2 && b != 4)
 					{
-						_putchar(a);
-						_putchar(b);
+						_putchar(a + 0);
+						_putchar(b + 0);
 						_putchar(':');
-						_putchar(c);
-						_putchar(d);
+						_putchar(c + 0);
+						_putchar(d + 0);
 						_putchar('\n');
 					}
 					else
