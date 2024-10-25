7e0282fe07c15c7585783a50e7bf9d7c4687fe80
Tefa95100 <damien.julien.95100@gmail.com>
2024-10-25 23:04:48 +0200

    Add exercise 11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+void print_to_98(int n)
+{
+	if (n < 98)
+		for (n; n <= 98; n++)
+		{
+			if (n < 98)
+			{
+				_putchar(n + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+				_putchar(n + '0')
+		}
+	else if ( n > 98)
+		for (n; n >= 98; n--)
+		{
+			if (n > 98)
+			{
+				_putchar(n + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+				_putchar(n + '0');
+		}
+	else if (n == 98)
+		_putchar(n + '0');
+}
