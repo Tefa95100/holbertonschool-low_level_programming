e5551a773569ec09f75f078b2ec5d9a82bdb3920
Tefa95100 <damien.julien.95100@gmail.com>
2024-10-25 19:12:09 +0200

    Add exercise 7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ *print_last_digit - search the lasti digit of an int
+ *@n: an int
+ *Return: the last digit of n
+ */
+int print_last_digit(int n)
+{
+	return (n % 10)
+}
