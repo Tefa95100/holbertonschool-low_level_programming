1fce0818a92e42b276cb36efb9e60ff67200296f
Tefa95100 <damien.julien.95100@gmail.com>
2024-10-25 16:33:46 +0200

    betty is now happy

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -2,13 +2,13 @@
 
 /**
  *_islower - Says if it's a lowercase or uppercase
- *@param: receive a letter
+ *@c: receive a letter
  *Return: 1 if the letter is lowercase else 0 if other
  */
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
-		return 1;
+		return (1);
 	else
-		return 0;
+		return (0);
 }
