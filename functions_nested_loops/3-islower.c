6badf9eec70fc92bfd538e6138fbd62dd2cf345c
Tefa95100 <damien.julien.95100@gmail.com>
2024-10-25 15:24:53 +0200

    Add exercise 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.c"
+
+/**
+ *_islower - Says if it's a lowercase or uppercase
+ *@param: receive a letter
+ *Return: 1 if the letter is lowercase else 0 if other
+ */
+int _islower(int c)
+{
+	if (c < 97 && c > 122)
+		return 1;
+	else
+		return 0,
+}
