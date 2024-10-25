2737e3828dcc184373163d93ca6a26ede538b2b0
Tefa95100 <damien.julien.95100@gmail.com>
2024-10-25 22:21:26 +0200

    Add exercise 9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+void times_table(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			return (a*b);
+		}
+	}
+}
