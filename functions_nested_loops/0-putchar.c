d58c72d010a6f55d0c938f0154e02f4bc8965924
Tefa95100 <damien.julien.95100@gmail.com>
2024-10-25 10:19:25 +0200

    push 0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+int _putchar(char);
+/**
+ *main - Entry point of programm
+ *Return: return 0
+ */
+
+int main(void)
+{
+	char text[10] = "_putchar\n";
+	int i;
+
+	for (i = 0; text[i] != '\0'; i++)
+	{
+		_putchar(text[i]);
+	}
+	return (0);
+}
