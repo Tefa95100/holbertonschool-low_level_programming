9f0c86864b7ad7c45ed2dd91c49105f3eb40576f
Tefa95100 <damien.julien.95100@gmail.com>
2024-10-25 23:06:34 +0200

    Add comma an remove statement with no effect

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -8,7 +8,7 @@
 void print_to_98(int n)
 {
 	if (n < 98)
-		for (n; n <= 98; n++)
+		for (; n <= 98; n++)
 		{
 			if (n < 98)
 			{
@@ -17,10 +17,10 @@ void print_to_98(int n)
 				_putchar(' ');
 			}
 			else
-				_putchar(n + '0')
+				_putchar(n + '0');
 		}
 	else if ( n > 98)
-		for (n; n >= 98; n--)
+		for (; n >= 98; n--)
 		{
 			if (n > 98)
 			{
