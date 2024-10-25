9383471ec2a8181bde89d1e559ddb42d997ca986
Tefa95100 <damien.julien.95100@gmail.com>
2024-10-25 20:17:52 +0200

    Add exercise 8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ *jack_bauer - print all number of 00:00 to 23:59
+ */
+void jack_bauer(void)
+{
+	int a;
+	int b;
+	int c;
+	int d;
+
+	for (a = 0; a < 3; a++)
+	{
+		for (b = 0; b < 4; b++)
+		{
+			for (c = 0; c < 6; c++)
+			{
+				for (d = 0; d <= 9; d++)
+				{
+					if (a != 2 && b != 4)
+					{
+						_putchar(a);
+						_putchar(b);
+						_putchar(':');
+						_putchar(c);
+						_putchar(d);
+					}
+					else
+						break;
+				}
+			}
+		}
+	}
+}
