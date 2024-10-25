b2efef47d3069daab6295d8ac09018d7cc445404
Tefa95100 <damien.julien.95100@gmail.com>
2024-10-25 13:25:09 +0200

    it's just a function

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -8,10 +8,9 @@ void print_alphabet(void)
 {
 	char alphabet[27] = "abcdefghijklmnopqrstuvwxyz\n";
 	int i;
-	
-	for(i = 0; alphabet[i] != '\0'; i++)
+
+	for (i = 0; alphabet[i] != '\0'; i++)
 	{
 		_putchar(alphabet[i]);
 	}
-	return (0);
 }
