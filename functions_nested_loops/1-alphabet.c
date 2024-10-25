9f398c0695052733bfa9f796d938418c3db98b70
Tefa95100 <damien.julien.95100@gmail.com>
2024-10-25 11:16:52 +0200

    push the main with all prototype and exercise 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point of programm
+ * Return: return 0
+ */
+int main (void)
+{
+	char alphabet[27] = "abcdefghijklmnopqrstuvwxyz\n";
+	int i;
+
+		for(i = 0, alphabet[i] != '\0'; i++)
+		{
+			putchar(alphabet[i]);
+		}
+	return (0);
+}
