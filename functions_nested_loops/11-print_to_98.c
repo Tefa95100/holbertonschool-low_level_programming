f632dd179be7ad280a170a4ec5daa32631985028
Tefa95100 <damien.julien.95100@gmail.com>
2024-10-26 11:46:34 +0200

    Correction for -100

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -24,7 +24,7 @@ void print_to_98(int n)
 				_putchar(',');
 				_putchar(' ');
 			}
-			else if ((n > -100 && n < -9) || (n > 9 && n < 98))
+			else if ((n >= -100 && n < -9) || (n > 9 && n < 98))
 			{
 				print_2_digit(n);
 				_putchar(',');
