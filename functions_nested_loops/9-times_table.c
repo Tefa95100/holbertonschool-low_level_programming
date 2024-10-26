c834de5d3622fad303481aaa9859568e747da447
Tefa95100 <damien.julien.95100@gmail.com>
2024-10-26 07:46:59 +0200

    Correction for exercise 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,20 +1,34 @@
 #include "main.h"
 
 /**
- *
- *
- *
+ *times_table - display multiplication table
  */
 void times_table(void)
 {
 	int a;
 	int b;
+	int prod;
+	int affichage;
 
 	for (a = 0; a <= 9; a++)
 	{
 		for (b = 0; b <= 9; b++)
 		{
-			return (a*b);
+			prod = a * b;
+			if (prod > 9)
+			{
+				affichage = prod / 10;
+				_putchar(affichage + '0');
+			}
+			
+			affichage = prod % 10;
+			_putchar(affichage + '0');
+			if (b < 9)
+			{
+			_putchar(',');
+			_putchar(' ');
+			}
 		}
+		_putchar('\n');
 	}
 }
