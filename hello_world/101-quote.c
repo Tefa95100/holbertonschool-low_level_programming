d4f121517a5392f65abea70de333d8c595cd1efa
Tefa95100 <damien.julien.95100@gmail.com>
2024-10-26 18:11:12 +0200

    Add second advanced task

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  *
@@ -6,11 +6,13 @@
  */
 int main(void)
 {
-	char phrase[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	int i;
+	char phrase = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	for (i = 0; phrase[i] != '\0'; i++)
-		putchar(phrase[i]);
+	/*
+	 * for (i = 0; phrase[i] != '\0'; i++)
+	*	putchar(phrase[i]);
+	*/
+	write(1, phrase, 50);
 	return (1);
 
 }
