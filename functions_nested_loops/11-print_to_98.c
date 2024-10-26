328dcd4b7ed07b5106b42806f33f99ceb0e389f0
Tefa95100 <damien.julien.95100@gmail.com>
2024-10-26 11:45:18 +0200

    Add other function for less line in one function

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -5,28 +5,70 @@
  *@n: numbers for start
  *Return: return a count start to n to 98
  */
+
+/*Prototype*/
+void print_to_98(int n);
+void print_3_digit(int n);
+void print_2_digit(int n);
+
 void print_to_98(int n)
 {
-	int result;
-	int result_deux;
+	int p;
 
 	if (n <= 98)
 		for (; n <= 98; n++)
 		{
 			if (n < -100)
+			{
 				print_3_digit(n);
-			else if (n > -100 && n < -9 || n > 9 && n < 99)
+				_putchar(',');
+				_putchar(' ');
+			}
+			else if ((n > -100 && n < -9) || (n > 9 && n < 98))
+			{
 				print_2_digit(n);
+				_putchar(',');
+				_putchar(' ');
+			}
 			else if (n > -10 && n < 10)
-				_putchar(n);
+			{
+				if (n < 0)
+				{
+					_putchar('-');
+					p = n * -1;
+					_putchar(p + '0');
+				}
+				else
+					_putchar(n + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				print_2_digit(n);
+				_putchar('\n');
+			}
 		}
 	else if (n >= 98)
 		for (; n >= 98; n--)
 		{
-			if (n > 100)
+			if (n >= 100)
+			{
 				print_3_digit(n);
-			else
+				_putchar(',');
+				_putchar(' ');
+			}
+			else if (n != 98)
+			{
 				print_2_digit(n);
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				print_2_digit(n);
+				_putchar('\n');
+			}
 		}
 }
 
@@ -36,6 +78,14 @@ void print_to_98(int n)
  */
 void print_3_digit(int n)
 {
+	int result;
+	int result_deux;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * -1;
+	}
 	result = n / 100;
 	_putchar(result + '0');
 	result_deux = (n - (result * 100)) / 10;
@@ -50,6 +100,13 @@ void print_3_digit(int n)
  */
 void print_2_digit(int n)
 {
+	int result;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * -1;
+	}
 	result = n / 10;
 	_putchar(result + '0');
 	result = n % 10;
