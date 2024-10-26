9a84a74f9cda95eff5c85df7320b879b9221fab5
Tefa95100 <damien.julien.95100@gmail.com>
2024-10-26 18:17:48 +0200

    Correction use write not a array

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -6,13 +6,9 @@
  */
 int main(void)
 {
-	char phrase = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	const char *phrase = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	/*
-	 * for (i = 0; phrase[i] != '\0'; i++)
-	*	putchar(phrase[i]);
-	*/
-	write(1, phrase, 50);
+	write(1, phrase, 59);
 	return (1);
 
 }
