cf07d4891ac39ad0b0f2ec93b89c1fe7ebee56d6
Tefa95100 <damien.julien.95100@gmail.com>
2024-10-26 11:01:15 +0200

    Correction

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,36 +1,57 @@
 #include "main.h"
 
 /**
- *
- *
- *
+ *print_to_98 - print all numbers to n to 98
+ *@n: numbers for start
+ *Return: return a count start to n to 98
  */
 void print_to_98(int n)
 {
-	if (n < 98)
+	int result;
+	int result_deux;
+
+	if (n <= 98)
 		for (; n <= 98; n++)
 		{
-			if (n < 98)
-			{
-				_putchar(n + '0');
-				_putchar(',');
-				_putchar(' ');
-			}
-			else
-				_putchar(n + '0');
+			if (n < -100)
+				print_3_digit(n);
+			else if (n > -100 && n < -9 || n > 9 && n < 99)
+				print_2_digit(n);
+			else if (n > -10 && n < 10)
+				_putchar(n);
 		}
-	else if ( n > 98)
+	else if (n >= 98)
 		for (; n >= 98; n--)
 		{
-			if (n > 98)
-			{
-				_putchar(n + '0');
-				_putchar(',');
-				_putchar(' ');
-			}
+			if (n > 100)
+				print_3_digit(n);
 			else
-				_putchar(n + '0');
+				print_2_digit(n);
 		}
-	else if (n == 98)
-		_putchar(n + '0');
+}
+
+/**
+ *print_3_digit - take n and print all digit with _putchar()
+ *@n: the numbers for print
+ */
+void print_3_digit(int n)
+{
+	result = n / 100;
+	_putchar(result + '0');
+	result_deux = (n - (result * 100)) / 10;
+	_putchar(result_deux + '0');
+	result = n % 10;
+	_putchar(result + '0');
+}
+
+/**
+ *print_2_digit - take n and print all digit with _putchar()
+ *@n: the numbers for print
+ */
+void print_2_digit(int n)
+{
+	result = n / 10;
+	_putchar(result + '0');
+	result = n % 10;
+	_putchar(result + '0');
 }
