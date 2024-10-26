e43ff4c77d6579a3c2adf2ce70991fa7cb531bdf
Tefa95100 <damien.julien.95100@gmail.com>
2024-10-26 18:26:24 +0200

    Add a space

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -9,7 +9,7 @@ int main(void)
 	const char *phrase = "and that piece of art is useful\" ";
 	const char *phrase2 = "- Dora Korpar, 2015-10-19\n";
 
-	write(1, phrase, 32);
+	write(1, phrase, 33);
 	write(1, phrase2, 26);
 	return (1);
 
