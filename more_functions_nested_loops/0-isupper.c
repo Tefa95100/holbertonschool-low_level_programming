47cdb26050e9369b98f03abbede899cb6affe9a4
Tefa95100 <damien.julien.95100@gmail.com>
2024-10-28 07:42:51 +0100

    Add exercise 0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - check if c is Uppercase
+ * @c: a character ASCII
+ * Return: return 1 if succes else return 0
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <=90)
+		return (1);
+	else
+		return (0);
+}
