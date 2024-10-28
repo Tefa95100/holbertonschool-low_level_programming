938d319b1f809df23e852be2b79d2d83ddb852c1
Tefa95100 <damien.julien.95100@gmail.com>
2024-10-28 10:55:58 +0100

    Remove a space

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -8,7 +8,7 @@ void print_most_numbers(void)
 {
 	int count;
 
-	for (count = 0; count <= 10; count ++)
+	for (count = 0; count <= 10; count++)
 	{
 		if (count != 2 && count != 4 && count < 10)
 			_putchar(count + '0');
