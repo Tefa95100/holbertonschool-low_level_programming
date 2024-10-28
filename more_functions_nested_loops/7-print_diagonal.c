58042806eae4fab5cda0211e39c690ddbaddbac1
Tefa95100 <damien.julien.95100@gmail.com>
2024-10-28 13:48:31 +0100

    Correction for inccorect instruction

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -8,11 +8,19 @@
 void print_diagonal(int n)
 {
 	int count;
+	int space;
 
 	if (n > 0)
 	{
 		for (count = 0; count != n; count++)
+		{
+			for (space = 0; space < count; space++)
+			{
+				_putchar(' ');
+			}
 			_putchar('\\');
+			_putchar('\n');
+		}
 	}
 	_putchar('\n');
 }
