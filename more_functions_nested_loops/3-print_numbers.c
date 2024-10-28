c77bd287a3bc9d20e9f4d478b7dad0fe2b9d88d6
Tefa95100 <damien.julien.95100@gmail.com>
2024-10-28 10:38:41 +0100

    Add exercise 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *print_numbers - prints all digit with nex line
+ *Return: nothing
+ */
+void print_numbers(void)
+{
+	int count;
+
+	for (count = 0; count <= 10; count++)
+		if ( count < 10)
+			_putchar(count + '0');
+		else
+			_putchar('\n');
+}
