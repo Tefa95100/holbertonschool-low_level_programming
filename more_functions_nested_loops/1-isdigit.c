d3d7ed95590fc167c19511d94a8c184a290e01cb
Tefa95100 <damien.julien.95100@gmail.com>
2024-10-28 07:47:14 +0100

    Add exercise 1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - check if c is a digit
+ * @c: a character ASCII
+ * Return: return 1 if succes else return 0
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
