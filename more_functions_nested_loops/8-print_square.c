5986a888b4af84be673c897b8de96e97de2a8775
Tefa95100 <damien.julien.95100@gmail.com>
2024-10-28 15:21:25 +0100

    Add exercise 8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_square - print a square with '#'
+ * @size: size of square
+ * Return: nothing
+ */
+void print_square(int size)
+{
+	int column;
+	int line;
+
+	if (size > 0)
+	{
+		for (line = 0; line >= size; line++)
+		{
+			for (column = 0; column >= size; column++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
