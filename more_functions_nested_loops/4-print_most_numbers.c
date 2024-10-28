c15550f24dcb1758b2998020246ad524452f8b1a
Tefa95100 <damien.julien.95100@gmail.com>
2024-10-28 10:55:13 +0100

    Add exercise 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *print_most_numbers - print all numbers except 2 and 4
+ *Return: nothing
+ */
+void print_most_numbers(void)
+{
+	int count;
+
+	for (count = 0; count <= 10; count ++)
+	{
+		if (count != 2 && count != 4 && count < 10)
+			_putchar(count + '0');
+		else if (count == 10)
+			_putchar('\n');
+	}
+}
