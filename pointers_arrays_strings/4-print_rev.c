aeb3f27f1274ffcb30be99a8698c2e43488dff2e
Tefa95100 <damien.julien.95100@gmail.com>
2024-10-29 17:26:02 +0100

    decrement in first

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -10,9 +10,9 @@ void print_rev(char *s)
 	int count;
 
 	count = 0;
-	while ( s[count] != '\0')
+	while (s[count] != '\0')
 		count++;
-	for (; count >= 0; count--)
+	for (; count >= 0; --count)
 	{
 		_putchar(s[count]);
 	}
