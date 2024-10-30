4e0ebe33087cd354bdb0d8c42b3737e36f96e634
Tefa95100 <damien.julien.95100@gmail.com>
2024-10-30 23:14:52 +0100

    Add exercise 3 project 3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -19,6 +19,10 @@ int _strcmp(char *s1, char *s2)
 			count = count - 1;
 		else if (s1[i] > s2[i])
 			count = count + 1;
+		else if (s1[i] == '\0' && s2[i] != '\0')
+			count = count - 1;
+		else if (s1[i] != '\0' && s2[i] == '\0')
+			count = count + 1;
 	}
 	return (count);
 }
