2a95f5e7448d59a8cf0472201c7874b2818fe49d
Tefa95100 <damien.julien.95100@gmail.com>
2024-10-30 20:31:48 +0100

    Add exercise 2 project 3

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_strncpy - copy a string chain in function of n 
+ *@dest: destination of copy
+ *@src: source of copy
+ *@n: numbers of bytes to copy
+ *Return: return dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0' && i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
