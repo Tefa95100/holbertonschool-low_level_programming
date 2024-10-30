c521afb0255e47e451f2273035c422c42b1b8ce1
Tefa95100 <damien.julien.95100@gmail.com>
2024-10-30 08:53:28 +0100

    Add exercise 7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *puts_half - print half of character
+ *@str: string chain
+ *Return: nothing
+ */
+void puts_half(char *str)
+{
+	int count;
+	int len;
+
+	len = 0;
+	while (str[len] != '\0')
+		len++;
+	for (count = 0; count <= (len - 1) / 2; count++)
+		_putchar(str[count]);
+	_putchar('\n');
+}
