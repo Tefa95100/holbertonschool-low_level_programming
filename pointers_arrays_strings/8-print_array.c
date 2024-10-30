781aeb744ffc7e2694d5e5620fb7a0998939e5be
Tefa95100 <damien.julien.95100@gmail.com>
2024-10-30 11:00:50 +0100

    Correction for print

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -11,9 +11,9 @@ void print_array(int *a, int n)
 {
 	int count;
 
-	for (count = 0; count <= n; count++)
+	for (count = 0; count < n; count++)
 	{
-		if (count < n)
+		if (count < n - 1)
 			printf("%d, ", a[count]);
 		else
 			printf("%d\n", a[count]);
