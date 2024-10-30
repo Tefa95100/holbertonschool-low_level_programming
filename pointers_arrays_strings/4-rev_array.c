46fe0c3edf115077adc0e4c50c22e14e82bc592d
Tefa95100 <damien.julien.95100@gmail.com>
2024-10-30 23:21:30 +0100

    Add exercise 4 project 3

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *reverse_array - reverse array of int
+ *@a: array of int
+ *@n: number of element in arrays
+ *Return: nothing
+ */
+void reverse_array(int *a, int n)
+{
+	int tmp;
+	int i;
+	
+	for (i = 0; i < n; i++)
+	{
+		tmp = a[i];
+		a[i] = a[n];
+		a[n] = tmp;
+	}
+}
