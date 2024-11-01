046079c66798242fbea2acbc180ec859675175f8
Tefa95100 <damien.julien.95100@gmail.com>
2024-11-01 10:19:46 +0100

    Correction for case or lenght source is less of n

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -10,15 +10,18 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	int i_max;
+	int dest_max;
+	int src_max
 
 	i_max = 0;
-	while (dest[i_max] != '\0')
+	while (dest[dest_max] != '\0')
 		i_max++;
 
-	for (i = 0; i < n && n < i_max; i++)
+	for (i = 0; i < n && n < dest_max; i++)
 	{
 		if (src[0] == '\0')
+			dest[i] = '\0';
+		else if (i > src_max)
 			dest[i] = '\0';
 		else
 			dest[i] = src[i];
