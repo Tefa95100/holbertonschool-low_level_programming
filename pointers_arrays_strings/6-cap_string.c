26d0beaaaa6b6f40f851845b6470df8f2dc918b5
Tefa95100 <damien.julien.95100@gmail.com>
2024-11-02 18:34:29 +0100

    Add exercise 6 project 3

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+char *cap_string(char *str)
+{
+	int i;
+	int isep;
+	char separator[] = " ,;.!?\"(){}\t\n";	/*Word separator for check*/
+
+	if (str[0] >= 97 && str[0] <= 122)	/*Search if the first character is Upper or lower*/
+		str[0] = str[0] - 32;
+	for (i = 1; str[i] != '\0'; i++)
+	{
+		for (isep = 0; separator[isep] != '\0'; isep++)
+		{
+			if (separator[isep] == str[i - 1])	/*Check if previous char is a separator*/
+			{
+				if (str[i] >=97 && str[i] <= 122)	/*check if a lowercase*/
+					str[i] = str[i] - 32;
+				break;
+			}
+		}
+	}
+	return (str);
+}
