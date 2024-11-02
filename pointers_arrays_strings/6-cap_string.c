eceaf6cc70664007160b725c2e0df43ea898c094
Tefa95100 <damien.julien.95100@gmail.com>
2024-11-02 18:38:38 +0100

    Betty compliant

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,12 +1,17 @@
 #include "main.h"
 
+/**
+ * *cap_string - transform character afeter a separator in Upper
+ *@str: string chain for transform
+ *Return: the string chain modify
+ */
 char *cap_string(char *str)
 {
 	int i;
 	int isep;
 	char separator[] = " ,;.!?\"(){}\t\n";	/*Word separator for check*/
 
-	if (str[0] >= 97 && str[0] <= 122)	/*Search if the first character is Upper or lower*/
+	if (str[0] >= 97 && str[0] <= 122)	/*Check if the first is lower*/
 		str[0] = str[0] - 32;
 	for (i = 1; str[i] != '\0'; i++)
 	{
@@ -14,7 +19,7 @@ char *cap_string(char *str)
 		{
 			if (separator[isep] == str[i - 1])	/*Check if previous char is a separator*/
 			{
-				if (str[i] >=97 && str[i] <= 122)	/*check if a lowercase*/
+				if (str[i] >= 97 && str[i] <= 122)	/*check if a lowercase*/
 					str[i] = str[i] - 32;
 				break;
 			}
