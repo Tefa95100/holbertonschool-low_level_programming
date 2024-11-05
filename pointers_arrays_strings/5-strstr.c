f39c2ffa6091c82960c4fbf79fd82f31aa20b14a
Tefa95100 <damien.julien.95100@gmail.com>
2024-11-05 15:39:31 +0100

    Exercise 5 complete

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  *
@@ -8,4 +9,36 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
+	int lenght = 0;
+	int previous_count = 0;
+	int i = 0;
+	int j = 0;
+	char *substring;
+
+	while (needle[lenght] != '\0')	/*Search the lenght of needle*/
+		lenght++;
+	while (haystack[i] != '\0')	/*Browse the string haystack*/
+	{
+		while (needle[j] != '\0')	/*Browse the string needle*/
+		{
+			if (haystack[i] == needle[j])	/*Search identical char for increment count*/
+			{
+				j++;
+				break;
+			}
+			else
+				break;
+		}
+		if (lenght == j)
+		{
+			substring = &haystack[i - lenght + 1];
+			return (substring);
+		}
+		if (j == previous_count)
+			j = 0;
+		else
+			previous_count = j;
+		i++;
+	}
+	return (NULL);
 }
