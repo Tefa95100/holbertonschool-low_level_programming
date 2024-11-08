bfed7ee4ee741c3bce947547e1b808654a1235fc
Tefa95100 <damien.julien.95100@gmail.com>
2024-11-08 17:05:37 +0100

    Betty compliant

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -5,9 +5,9 @@
  * @argv: put argument in array
  * Return: return 0 if success
  */
-int main(int argc,char *argv[])
+int main(int argc, char *argv[])
 {
-    (void)argc;
-    printf("%s\n", argv[0]);
-    return (0);
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
 }
--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -10,26 +10,26 @@
  */
 int main(int argc, char *argv[])
 {
-    int i;
-    int number;
-    int sum = 0;
-    int previous_sum = 0;
+	int i;
+	int number;
+	int sum = 0;
+	int previous_sum = 0;
 
-    if (argc == 1)
-    {
-        printf("0\n");
-    }
-    for (i = 0; i < argc; i++)
-    {
-        number = atoi(argv[i]);
-        sum = sum + number;
-        if (previous_sum == sum)    
-        {
-            printf("Error\n");
-            return (1);
-        }
-        previous_sum = sum;
-    }
-    printf("%d\n", sum);
-    return (0);
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	for (i = 0; i < argc; i++)
+	{
+		number = atoi(argv[i]);
+		sum = sum + number;
+		if (previous_sum == sum)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		previous_sum = sum;
+	}
+	printf("%d\n", sum);
+	return (0);
 }
