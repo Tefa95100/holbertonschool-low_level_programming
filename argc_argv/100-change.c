0a5e4e490eeca84a162b17b0f368bbc011194e63
Tefa95100 <damien.julien.95100@gmail.com>
2024-11-09 22:44:02 +0100

    Simplification by remove recursive and calculate in function coin

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -3,24 +3,6 @@
 #include <ctype.h>
 
 /**
- * number_coin - Search number coin by cent
- *@n: mount of money
- *@multi: number of coin
- *@cent: mount of cent
- * Return: return the multi -1
- */
-int number_coin(int n, int multi, int cent)
-{
-	if (n - (multi * cent) < 0)	/*Search if multi * cent exceed money*/
-	{
-		return (multi - 1);	/*Decrease multi before return*/
-	}
-	else
-	{
-		return (number_coin(n, multi + 1, cent));	/*Increment multi*/
-	}
-}
-/**
  * coin - count the number of coin
  *@money: mount of money
  *Return: the number of coins
@@ -29,19 +11,14 @@ int coin(int money)
 {
 	int cent[5] = {25, 10, 5, 2, 1};
 	int i;
-	int coin = 0;
-	int previous_coin = 0;
+	int coin_count = 0;
 
 	for (i = 0; i < 5; i++)
 	{
-		coin = coin + number_coin(money, 1, cent[i]);	/*Stock coin*/
-		if (coin != previous_coin)	/*Verify if coin is change*/
-		{
-			previous_coin = coin;	/*Stock the new value of coin*/
-			money = money - (coin * cent[i]);	/*Decrease money*/
-		}
+		coin_count = coin_count + (money / cent[i]); /*Calcul nbr of coin*/
+		money %= cent[i];	/*Calculate reste of money*/
 	}
-	return (coin);
+	return (coin_count);
 }
 /**
  * main- entry point
@@ -51,19 +28,8 @@ int coin(int money)
  */
 int main(int argc, char *argv[])
 {
-	int money, i, j;
+	int money;
 
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (!isdigit(argv[i][j]))	/*Control if argument is digit*/
-			{
-				printf("Error\n");
-				return (1);
-			}
-		}
-	}
 	if (argc != 2)	/*Control if receive 1 argument*/
 	{
 		printf("Error\n");
