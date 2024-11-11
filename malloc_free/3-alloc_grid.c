41bcadbe2fd44faca4f55d5a847b62c8f48ec1ef
Tefa95100 <183406157+Tefa95100@users.noreply.github.com>
2024-11-11 16:28:34 +0100

    betty compliant

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -29,7 +29,7 @@ int **alloc_grid(int width, int height)
 	}
 	for (i = 0; i < height; i++)	/*Initialize array*/
 	{
-		for ( j = 0; j < width; j++)
+		for (j = 0; j < width; j++)
 		{
 			int_array[i][j] = 0;
 		}
