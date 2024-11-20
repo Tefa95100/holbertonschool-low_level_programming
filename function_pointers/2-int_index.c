a9acd496375648122602ea45e2cb2db9a28758de
Tefa95100 <damien.julien.95100@gmail.com>
2024-11-20 10:11:36 +0100

    Add verification for array and cmp NULL

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -10,7 +10,14 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
-
+	if (array == NULL)
+	{
+		return (-1);
+	}
+	if (cmp == NULL)
+	{
+		return (NULL);
+	}
 	if (size <= 0)	/*check size for 0 or less*/
 	{
 		return (-1);
