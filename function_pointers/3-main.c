705d545d34f1d3560a799edc93f98dbef4595cc7
Tefa95100 <damien.julien.95100@gmail.com>
2024-11-20 13:01:29 +0100

    Add control for 2operator

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -24,6 +24,11 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
+	if (argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
 	first = atoi(argv[1]);
 	second = atoi(argv[3]);
 	printf("%d\n", get_op_func(argv[2])(first, second));	/*Print result*/
