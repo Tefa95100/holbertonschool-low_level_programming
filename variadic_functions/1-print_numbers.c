1d409321d9e57f03bef5c55523a43191542d1419
Tefa95100 <damien.julien.95100@gmail.com>
2024-11-20 15:26:06 +0100

    change for make nothin if n == 0

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -14,23 +14,21 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list nbr_print;
 	unsigned int i;
 
-	if (n == 0)
+	if (n != 0)
 	{
-		printf("Error");
-		exit(100);
-	}
-	va_start(nbr_print, n);	/*Initialize list*/
-	for (i = 0; i < n; i++)	/*Browse the list*/
-	{
-		printf("%d", va_arg(nbr_print, int));	/*Print the number*/
-		if (i < n - 1 && separator != NULL)
+		va_start(nbr_print, n);	/*Initialize list*/
+		for (i = 0; i < n; i++)	/*Browse the list*/
 		{
-			printf("%s", separator);	/*Print separator if is not NULL*/
+			printf("%d", va_arg(nbr_print, int));	/*Print the number*/
+			if (i < n - 1 && separator != NULL)
+			{
+				printf("%s", separator);	/*Print separator if is not NULL*/
+			}
+			else if (i == n - 1)
+			{
+				printf("\n");	/*Print new line at the ends*/
+			}
 		}
-		else if (i == n - 1)
-		{
-			printf("\n");	/*Print new line at the ends*/
-		}
+		va_end(nbr_print);	/*Clean the list*/
 	}
-	va_end(nbr_print);	/*Clean the list*/
 }
