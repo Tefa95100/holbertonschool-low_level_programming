40194b3074f5dfb28a5846040b354c500dc7673a
Tefa95100 <damien.julien.95100@gmail.com>
2024-12-06 23:32:03 +0100

    Add comment a free

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -10,18 +10,24 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int index_incrementation = 0;
-	dlistint_t *temp;
+	dlistint_t *temp = NULL;
 	dlistint_t *new_element = malloc(sizeof(*new_element));
 
+	/*Check if allocate is fail*/
 	if (new_element == NULL)
 	{
 		return (NULL);
 	}
+
+	/*Check if h is NULL*/
 	if (*h == NULL)
 	{
+		free(new_element);
 		return (NULL);
 	}
 	temp = *h;
+
+	/*Browse the list and search node by index*/
 	while (temp->next != NULL && index_incrementation != idx)
 	{
 		index_incrementation++;
