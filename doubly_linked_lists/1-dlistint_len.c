f3eaa9896a3f9e6d2df4ba1ddfcb338e788cc9cf
Tefa95100 <damien.julien.95100@gmail.com>
2024-12-06 15:24:28 +0100

    Add coment, initialize variable and simply code

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -8,13 +8,13 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-	int lenght;
+	int lenght = 0;
 
-	if (h == NULL)
+	if (h == NULL)	/*Check h is NULL*/
 	{
 		return (0);
 	}
-	while (h != NULL)
+	while (h)	/*Browse list*/
 	{
 		lenght++;
 		h = h->next;
