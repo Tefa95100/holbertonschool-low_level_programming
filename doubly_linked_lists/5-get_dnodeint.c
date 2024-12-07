28d1ca2b1c82a852d2e69d00d4a9659004478386
Tefa95100 <damien.julien.95100@gmail.com>
2024-12-07 10:48:01 +0100

    Add comment and exceptions for last node

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -10,14 +10,17 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int index_increment = 0;
 
+	/*Browse the list and search the node*/
 	while (head->next != NULL && index_increment != index)
 	{
 		head = head->next;
 		index_increment++;
 	}
-	if (head->next == NULL)
+	/*Return NULL if the ends of list and index is not find*/
+	if (head->next == NULL && index_increment != index)
 	{
 		return (NULL);
 	}
+	/*return the node find*/
 	return (head);
 }
