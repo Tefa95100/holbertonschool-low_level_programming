7672e531624dfc68ff66784a3b1f065c89d24458
Tefa95100 <damien.julien.95100@gmail.com>
2024-12-07 19:48:49 +0100

    Use other function for add node in begin or end

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -11,22 +11,17 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int index_incrementation = 0;
 	dlistint_t *temp = NULL;
-	dlistint_t *new_element = malloc(sizeof(*new_element));
+	dlistint_t *new_element = malloc(sizeof(new_element));
 
 	if (new_element == NULL)	/*Check if allocate is fail*/
 		return (NULL);
-
 	new_element->n = n;	/*Add n in new element*/
 	if (idx == 0)	/*if new_node is the first element h point newt_element*/
 	{
-		if (*h == NULL)	/*If h is NULL*/
-			new_element->next = NULL;
-
-		*h = new_element;
-		new_element->prev = NULL;
+		new_element = add_dnodeint(h, n);
 		return (new_element);
 	}
-	if (*h == NULL)	/*Check if h null and idx > 0*/
+	if (new_element == NULL)	/*Check if h null and idx > 0*/
 	{
 		free(new_element);
 		return (NULL);
@@ -41,6 +36,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		free(new_element);
 		return (NULL);
+	}
+	if (temp->next == NULL)
+	{
+		add_dnodeint_end(h, n);
+		return (new_element);
 	}
 	/*Attribute variable for insertion of new_element*/
 	new_element->next = temp;
