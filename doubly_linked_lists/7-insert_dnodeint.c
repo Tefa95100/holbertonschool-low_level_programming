741e0bea27b443147e985dbc077f6079db3cb221
Tefa95100 <damien.julien.95100@gmail.com>
2024-12-07 17:41:04 +0100

    Change position of verification for h NULL and index 0

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -21,6 +21,15 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	/*Check if h is NULL*/
 	if (*h == NULL)
 	{
+		/*if the new node is the first element h point newt_element*/
+		if (idx == 0)
+		{
+			new_element->n = n;
+			new_element->next = NULL;
+			new_element->prev = NULL;
+			*h = new_element;
+			return (new_element);
+		}
 		free(new_element);
 		return (NULL);
 	}
@@ -43,10 +52,5 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	temp->prev = new_element;
 	temp = new_element->prev;
 	temp->next = new_element;
-	/*if the new node is the first element h point newt_element*/
-	if (idx == 0)
-	{
-		*h = new_element;
-	}
 	return (new_element);
 }
