da8a9b6b155f8742aecd58437a364e33fa3e922d
Tefa95100 <damien.julien.95100@gmail.com>
2024-12-07 12:43:02 +0100

    Add comment

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -8,16 +8,18 @@
 int sum_dlistint(dlistint_t *head)
 {
 	int sum = 0;
-
+	/*Check if head is NULL*/
 	if (head == NULL)
 	{
 		return (0);
 	}
+	/*Browse the list and integer in the sum*/
 	while (head->next != NULL)
 	{
 		sum += head->n;
 		head = head->next;
 	}
+	/*Add the integer in last node before return the sum*/
 	sum += head->n;
 	return (sum);
 }
